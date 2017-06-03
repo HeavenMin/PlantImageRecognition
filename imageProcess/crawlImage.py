5dfe1bad814e9bb20bdf14ab819b642f09331bc8
HeavenMin <ming1@student.unimelb.edu.au>
2017-06-03 18:12:09 +1000

    sortOut

--- imageProcess/crawlImage.py
+++
@@ -1,8 +0,0 @@
-from icrawler.builtin import GoogleImageCrawler
-
-google_crawler = GoogleImageCrawler(parser_threads=2, downloader_threads=4,
-                                    storage={'root_dir' : '/home/ubuntu/mingao/crawlImage'})
-google_crawler.crawl(keyword='rose', max_num=1000,
-                     date_min=None, date_max=None,
-                     min_size=(200,200), max_size=None)
-
